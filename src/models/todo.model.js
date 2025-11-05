0cc180c9e8df3f3e117f5adb0e69c4e6f37f263d
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-11-05 19:33:18 +0530

    setted Schema methods

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -19,5 +19,33 @@ const todoSchema = new mongoose.Schema({
 },{timestamps:true}
 );
 
+
+todoSchema.statics.getTodosByUser = async function(userId){
+    return await this.find({owner: userId }).sort({createdAt:-1})
+}
+
+todoSchema.statics.createTodoForUser=async function(userId,title){
+    const todo = await this.create({title,owner: userId});
+    return todo;
+}
+
+todoSchema.statics.deleteTodoByUser = async function (userId,todoId){
+    const todo = await this.findOneAndDelete({_id:todoId,owner:userId});
+    return todo;
+
+};
+
+todoSchema.statics.updateTodoByUser = async function (userId,todoId,updates){
+    const todo = await this.findOneAndUpdate({
+        _id: todoId, owner: userId
+    },
+updates,
+{new:true,runValidators: true});
+return todo;
+}
+
+
+
+
 export const todo = mongoose.model("todo",todoSchema)
 
